2ff64e61932f7437083ff4f7927d0366b03e4575
khushalchopraa <advkhushal97@gmail.com>
2024-08-21 21:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+ipt type="text/javascript" src="./script.js">
+		const student={
+			name: "mohan"
+		}
+		Object.prototype.getkeys= function(){ 
+			return Object.keys(this)
+		};
+		console.log(student.getkeys());
